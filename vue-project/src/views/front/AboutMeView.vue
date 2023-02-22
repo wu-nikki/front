69c24eb51b390c84ce03c67b121c758274c35e88
wu-nikki <nikki851205@gmail.com>
2023-02-22 22:21:45 +0800

    友善資訊OK，關於我們一半

--- vue-project/src/views/front/AboutMeView.vue
+++ vue-project/src/views/front/AboutMeView.vue
@@ -1,16 +1,108 @@
 <template>
-當您走進 MouHi 的網站，您會發現一個充滿愛心和關懷的社群。MouHi 是一個致力於讓每一隻可愛的毛孩都能找到一個溫馨的家庭的網站。
+  <div class="container">
+    <div class="photo">
+      <img src="../../assets/aboutMe/1.1.jpg" class="dog1" />
+      <img src="../../assets/aboutMe/2.1.jpg" class="dog2" />
+      <img src="../../assets/aboutMe/3.1.jpg" class="cat1" />
+    </div>
 
-我們的名字MouHi就像一隻毛孩的名字，讓人聯想到可愛、親密和溫暖。在 MouHi，我們相信每一隻毛孩都值得一個美好的家庭。因此，我們提供一個平台，讓毛孩和有愛心的人們可以輕鬆地連接起來。
-
-通過我們的網站，您可以輕鬆地搜索最適合您的毛孩，並進行認養程序。我們的平台不僅讓您可以與潛在的認養家庭聯繫，還提供了有關認養的豐富資源和指導，讓您準備好接納新的家庭成員。
-
-MouHi 為認養毛孩的人們提供一個安全、友好和開放的環境，讓您可以與志同道合的人們互動和分享經驗。在我們的社群中，您可以找到有用的資訊和支持，並與其他動物愛好者建立有意義的關係。
-
-請加入 MouHi 的社群，與我們一起為認養毛孩的事業努力。在 MouHi，我們相信愛是無價的，而且每一隻毛孩都值得一個幸福的家庭。
-
+    <h1>MOUHi，我們相信愛是無價的<br />每一隻毛孩都值得一個幸福的家庭。</h1>
+    <p>
+      當您走進 MOUHi
+      的網站，您會發現這是致力於讓每一隻毛孩都能找到一個溫馨家庭的網站。
+      MOUHi就像一隻毛孩的名字，讓人聯想到可愛、親密和溫暖。<br />
+      通過我們的網站，讓毛孩和有愛心的人們可以輕鬆地連接起來，您可以輕鬆地搜索最適合您的毛孩，並加入毛孩收藏。我們的網站不僅提供了有關毛孩友善資訊的認養資源，在往後還能讓您可以預約收容所，讓您準備好接納新的家庭成員。
+    </p>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container {
+  overflow: hidden;
+  width: 100%;
+  height: 91vh;
+}
+
+h1 {
+  margin-left: 20px;
+  padding: 2vw 3vw 2vw 4vw;
+  font-size: calc(22px + 1.4vw);
+  color: rgb(255, 108, 63);
+}
+p {
+  margin-left: 20px;
+  padding-left: 4vw;
+}
+
+.photo {
+  float: right;
+  width: 45vw;
+  height: 90%;
+  position: relative;
+
+  .dog1 {
+    width: calc(100px + 7vw);
+    border-radius: 20vw;
+    position: absolute;
+    top: calc(0px + 3vw);
+    right: 1vw;
+  }
+  .cat1 {
+    width: calc(150px + 10vw);
+    border-radius: 400px;
+    position: absolute;
+    top: calc(190px - 5vw);
+    right: calc(10px + 15vw);
+  }
+  .dog2 {
+    width: calc(150px + 3vw);
+    border-radius: 400px;
+    position: absolute;
+    bottom: -3vw;
+    right: calc(150px - 8vw);
+  }
+}
+@media (max-width: 768px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+  .photo {
+    width: 100%;
+    height: 50%;
+    .dog1 {
+      width: calc(150px + 7vw);
+      top: calc(10px + 5vw);
+      left: calc(30px + 3vw);
+    }
+    .cat1 {
+      width: calc(250px - 10vw);
+      top: calc(120px + 3vw);
+      right: calc(150px + 8vw);
+    }
+    .dog2 {
+      top: 0vw;
+      right: calc(20px + 5vw);
+    }
+  }
+}
+@media (max-width: 545px) {
+  .photo {
+    .dog1 {
+      top: 20px;
+      left: 20px;
+    }
+    .cat1 {
+      width: 150px;
+      top: 140px;
+      right: 150px;
+    }
+    .dog2 {
+      top: 20px;
+      right: 20px;
+    }
+  }
+}
+</style>
 
 <script setup></script>
--- vue-project/src/views/front/FriendlyInformation.vue
+++ vue-project/src/views/front/FriendlyInformation.vue
@@ -53,15 +53,79 @@
     </n-grid>
   </div>
   <div id="FriendlyList">
-    <n-card>
-      <n-h4>h4 标签</n-h4>
-      <n-p>
-        《且听风吟》是日本作家村上春树的第一本小说。它首次出现在 1979 年 6
-        月的《群像》（日本最有影响力的文学杂志之一），并于次月出版成书。这部小说被日本导演
-        Kazuki Ōmori 改编成电影，并于 1981 年由艺术剧院协会发行。1987
-        年被阿尔弗雷德伯恩鲍姆译成英文。
-      </n-p>
-    </n-card>
+    <a
+      href="https://animal.coa.gov.tw/Frontend/AdoptSearch/Detail/A000000001"
+      target="_blank"
+    >
+      <n-card hoverable>
+        <n-h4>讓牠擁有被愛的幸福：擁有寵物的方式Ⅰ - 認養</n-h4>
+        <n-p :line-clamp="2">
+          當我們有能力也想飼養寵物時，不妨先想想是不是一定要養所謂的純種名狗、名貓？犬貓品種真的是最重要的考量嗎?真的要花錢買嗎？許多流浪的狗狗貓貓，只要稍加整理，再加上您的愛心照顧，也能變的可愛又貼心，而您的生活也會因為牠們的陪伴變得更美好、更快樂！
+          被收容的流浪犬貓更需要您的關懷與照護，請至各地公立動物收容所認養犬貓，以認養代替購買，因為您的愛，讓流浪動物都能有個溫暖的新家，讓牠再度擁有被愛的幸福！
+          除了到公立收容所之外，您也可以留意各縣市政府或動保團體辦理的宣導及認養活動，有時候還會有免費植晶片或施打疫苗等優惠措施喔！</n-p
+        >
+      </n-card>
+    </a>
+
+    <a
+      href="https://animal.coa.gov.tw/Frontend/AdoptSearch/Detail/A000000002"
+      target="_blank"
+    >
+      <n-card hoverable>
+        <n-h4>讓牠來自有愛的地方：擁有寵物的方式Ⅱ - 合法購買</n-h4>
+        <n-p>
+          在您決定要飼養寵物犬隻，而且想要採取購買的方式時，切記！一定要選擇領有許可證的合法店家，在夜市、路邊流動攤販或是網路上購買來路不明的寵物，就是助長非法買賣。
+          您知道嗎？全台有許多非法的寵物繁殖業者為了高報酬獲利，藉由不人道的繁殖方式（如：注射藥物），讓母狗大量繁殖，同時也造成母狗快速老化。對於不能生的母狗，非法店家為節省成本往往將犬隻拋棄，甚至餓死；而較難售出的犬隻，也常採取同樣的處理方式。
+          購買寵物請向領有特定寵物業許可證的合法店家購買，才能避免非法店家的壯大，並記得與店家簽訂寵物買賣契約（條件依買賣雙方合意即可），當購買到生病的寵物或是消費有所糾紛的時候，才能有所依據，否則只能傷財也傷心。
+          透過向合法店家購買犬隻，讓您對牠的愛，愛屋及烏，消費更有保障。</n-p
+        >
+      </n-card>
+    </a>
+    <a
+      href="https://animal.coa.gov.tw/Frontend/AdoptSearch/Detail/A000000003"
+      target="_blank"
+    >
+      <n-card hoverable>
+        <n-h4>愛 不要只是一時：終養不棄養的宣導</n-h4>
+        <n-p>
+          人的生活有許多層面：工作、休閒、玩樂…，雖然寵物只是我們生活中的一部分，但是對於寵物而言，您就是牠的全世界，牠的生活都圍繞著您。
+          狗狗與貓貓都擁有豐富的情感，您的照顧及愛護會讓牠們感到幸福而變漂亮，但您的遺棄則會造成牠們一輩子的陰影；當您決定飼養寵物時，一定要做好照顧牠一輩子的準備，不要因為生病或不想負責任的時候就隨意拋棄，如果真的無法繼續飼養，也要將牠交託到可以照顧牠、愛牠的人手中。基於保護動物，尊重生命，請不要隨意放生寵物、家畜、家禽，如果違法就會依動物保護法處罰喔！
+          飼養寵物是一種對生命的責任與承諾，不要因為一時的興趣，沒有經過深思熟慮就飼養寵物，為了牠，請您做一個有責任感的飼主。
+          我們不一定要領養流浪動物，但我們可以不製造流浪動物，不是嗎？</n-p
+        >
+      </n-card>
+    </a>
+    <a
+      href="https://animal.coa.gov.tw/Frontend/AdoptSearch/Detail/A000000004"
+      target="_blank"
+    >
+      <n-card hoverable>
+        <n-h4>愛牠 就給牠專屬名份：寵物登記與植入晶片</n-h4>
+        <n-p>
+          <n-text >辦理寵物登記，愛犬走失協尋有依據</n-text>
+          為了讓大家有終身照顧寵物的觀念、提升飼主的責任及有效杜絕棄犬行為，政府特別設立寵物登記制度，規定所有犬隻皆需植入晶片上網辦理登記，還設置寵物登記管理資訊網站(http://www.pet.gov.tw)，加強便民服務，並於網站中設置犬隻遺失協尋功能。
+          不怕一萬，只怕萬一，家中寵物寶貝一旦走失，真的讓人心急如焚，飼主可至全國各寵物登記站，為您的愛犬植入晶片、登記身分，確保走失時有協尋依據。
+          <br /><n-text >【寵物登記流程】</n-text> 1.
+          親自攜帶寵物犬隻至寵物登記站，完成晶片注射。 2.
+          所需資料：飼主身分證明文件。
+          3.登記站將資料鍵入網站建檔，並繳交登記費後完成登記。</n-p
+        >
+      </n-card>
+    </a>
+
+    <a
+      href="https://animal.coa.gov.tw/Frontend/AdoptSearch/Detail/A000000005"
+      target="_blank"
+    >
+      <n-card hoverable>
+        <n-h4>愛 剛剛好就好：寵物絕育結紮</n-h4>
+        <n-p>
+          「絕育」是防止寵物繁殖後代的外科手術。為什麼需要絕育呢？
+          家裡的狗貓忽然生了一窩小狗小貓，常常會讓飼主很頭痛，不能上網賣，也不一定送得出去，那這些生命怎麼辦？難道要把牠們放到野外讓牠們淪為流浪動物？然後讓政府用納稅人交的錢來解決您造成的問題，再害政府被動保人士指責為何要安樂死這些動物？當然不行這麼做，其實無論您家裡飼養的狗貓是公的還是母的，只要幫牠絕育，您就不會有這些困擾，絕育還能防止狗貓年紀大了之後罹患繁殖器官的疾病喔！
+          絕育能讓我們的愛，剛剛好，這樣才能讓每隻寵物都得到妥善的照顧。</n-p
+        >
+      </n-card>
+    </a>
   </div>
 </template>
 
@@ -81,16 +145,14 @@
   }
 
   a {
-    width: 100%;
-    // flex-grow: 1;
+    max-width: 350px;
+    flex-grow: 1;
     text-decoration: none;
-    color: black;
-    background-color: aqua;
   }
   .n-card {
     width: 100% !important;
     // max-width: 350px;
-background: #000;
+
     height: 280px;
     cursor: pointer;
     img {
@@ -105,9 +167,40 @@ background: #000;
 #FriendlyList {
   margin: auto;
   width: 80%;
+  margin-bottom: 5vw;
+  a {
+    text-decoration: none;
+  }
   .n-card {
     max-width: 100%;
-    height: 10%;
+    height: 10rem;
+    padding: 1rem;
+    margin-bottom: 2vw;
+  }
+
+  .n-h4 {
+    padding-bottom: 3px;
+    margin-bottom: 0px;
+    font-size: 22px;
+    font-weight: 800;
+  }
+  .n-p {
+    margin-top: 5px;
+    font-size: 14px;
+    font-weight: normal;
+    text-align: left;
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+  .n-text{
+    font-weight: bolder;
+    font-size: 17px;
+  }
+  }
+  .n-card__content {
+    padding: 0px;
   }
 }
 
@@ -118,15 +211,10 @@ background: #000;
   #FriendlyCard {
     .n-grid {
       margin-bottom: 3rem;
-      div {
-        // margin: inherit;
-      }
-      .n-gi {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-     
+    }
+  }
+  #FriendlyList {
+    .n-card {
     }
   }
 }

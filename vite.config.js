865f2a4f32db2c4b311aafa24e91f9bdb829bcfd
wu-nikki <nikki851205@gmail.com>
2023-02-26 15:56:05 +0800

    把檔案往前移

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [vue(),AutoImport({
     imports: [
       'vue',
